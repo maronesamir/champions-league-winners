be41869e728f59593d50bb63daff2029ef232368
Marone samir <marouane.doukali92@gmail.com>
2019-12-21 22:46:44 +0100

    without percent

--- prepare.js
+++ prepare.js
@@ -1,23 +1,22 @@
 function prepareData(data) {
-	console.log("perpareData");
-	var map = new Map();
-	var mapResult = new Map();
-	var sum = 0;
-	for (var i = 0; i < data.length; i++) {
-		var wonValue = data[i].won;
-		var countryValue = data[i].country;
-		var value = map.get(countryValue);
-		sum = sum + wonValue;
-		if (value == null) {
-			map.set(countryValue, wonValue);
-		} else {
-			var newValue = value + wonValue;
-			map.set(countryValue, newValue);
-		}
-	}
-	var percent = (100 / sum);
-	map.forEach((value, key, map) => {
-		mapResult.set(key, (value * percent))
-	});
-	return new Map([...mapResult.entries()].sort((a, b) => b[1] - a[1]));
+    console.log("perpareData");
+    var map = new Map();
+    var mapResult = new Map();
+    
+    for (var i = 0; i < data.length; i++) {
+        var wonValue = data[i].won;
+        var countryValue = data[i].country;
+        var value = map.get(countryValue);
+        if (value == null) {
+            map.set(countryValue, wonValue);
+        } else {
+            var newValue = value + wonValue;
+            map.set(countryValue, newValue);
+        }
+    }
+
+    map.forEach((value, key, map) => {
+        mapResult.set(key, value)
+    });
+    return new Map([...mapResult.entries()].sort((a, b) => b[1] - a[1]));
 }
